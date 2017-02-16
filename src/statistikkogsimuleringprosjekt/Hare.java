7cea5a53125f3f63f54a50b6cf031f7a9003d21f
Ole Martin T. Vad <omtvad@gmail.com>
2017-02-16 12:07:11 +0100

    Opprettet en klasse Hare i prosjektet

---
+++ src/statistikkogsimuleringprosjekt/Hare.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistikkogsimuleringprosjekt;
+
+/**
+ * This represents the Hare class of the eco-system
+ * 
+ * @author olemartintvad
+ */
+public class Hare {
+    private int length;
+    private int litterSize;
+    
+    public Hare (int length, int litterSize) {
+        this.length = length;
+        this.litterSize = litterSize;
+    }
+}
