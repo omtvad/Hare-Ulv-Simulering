4601e50660eb327c7157f56a5ce1b6bfef8afc0e
Ole Martin T. Vad <omtvad@gmail.com>
2017-02-16 12:06:51 +0100

    Opprettet en klasse Wolf i prosjektet

---
+++ src/statistikkogsimuleringprosjekt/Wolf.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistikkogsimuleringprosjekt;
+
+/**
+ * This class represents the Wolf class for the eco-system
+ * 
+ * @author olemartintvad
+ */
+public class Wolf {
+    
+}
